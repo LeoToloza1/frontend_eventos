091ca647b40475bf7edd56db66e663d0139f839c
Toloza  Leonel <leotoloza6@gmail.com>
2024-10-21 22:53:07 -0300

    haciendo los primeros componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AlertasService } from './services/alertas.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'app-gestion';
+  constructor(private alertasService: AlertasService) {}
+  mostrarToast() {
+    this.alertasService.mostrarToast('Hola mundo', 'success', 'Hola Mundo');
+  }
 }
