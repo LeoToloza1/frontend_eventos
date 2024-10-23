12d5d027ebd99fcad36afcf605b585861c9d48ab
Toloza  Leonel <leotoloza6@gmail.com>
2024-10-23 16:53:08 -0300

    estructurando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AlertasService } from './services/alertas.service';
+import { AlertasService } from './core/services/alertas.service';
 
 @Component({
   selector: 'app-root',
