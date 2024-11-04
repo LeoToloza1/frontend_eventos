71099cc5366d00e2f722e0b00067ffce40c9810e
Toloza  Leonel <leotoloza6@gmail.com>
2024-11-04 19:14:40 -0300

    mejorando

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -5,17 +5,38 @@ import { NavBarComponent } from '../../shared/nav-bar/nav-bar.component';
 import { AlertasService } from '../../core/services/alertas.service';
 import { AuthService } from '../../core/services/auth.service';
 import { Usuario } from '../../Interfaces/Usuario';
+import { FormsModule } from '@angular/forms'; // Importa FormsModule
+import { MatFormFieldModule } from '@angular/material/form-field'; // Importa MatFormFieldModule
+import { MatInputModule } from '@angular/material/input'; // Importa MatInputModule
 
 @Component({
   selector: 'app-perfil',
   standalone: true,
-  imports: [FooterComponent, NavBarComponent, CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FooterComponent,
+    NavBarComponent,
+  ],
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css',
 })
 export class PerfilComponent implements OnInit {
-  usuario: Usuario | undefined;
+  usuario: Usuario = {
+    id: 0,
+    dni: 0,
+    nombre: '',
+    apellido: '',
+    email: '',
+    telefono: 0,
+    password: '',
+    rol: { rol: '' },
+    rol_id: 0,
+  };
   activado = false;
+
   constructor(
     private alerta: AlertasService,
     private authService: AuthService
@@ -25,10 +46,17 @@ export class PerfilComponent implements OnInit {
     this.obtenerUsuario();
   }
 
-  editarPerfil() {}
-
-  abrirModal() {
+  editarPerfil() {
     this.activado = true;
+  }
+
+  guardarCambios() {
+    // this.alerta.mostrarToast(
+    //   'Guardando cambios...',
+    //   'info',
+    //   'Guardando cambios'
+    // );
+    this.activado = false;
   }
 
   obtenerUsuario() {
